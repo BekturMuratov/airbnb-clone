37741e840791e1a360cab21d59b42a07a9df1898
BekturMuratov <Muratov225@gmail.com>
2020-08-31 23:49:29 +0600

    card and footer in home page component is done!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
     <Header />
     <Home/>
+    <Footer />
     </div>
   );
 }
