bf0ae574258cceea724f97e625d8b2414dfe57d8
BekturMuratov <Muratov225@gmail.com>
2020-09-01 01:08:28 +0600

    search page and search results components is donegit commit -m 'search page and search results components is done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="app">
+    <Router>
     <Header />
-    <Home/>
+    <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+    
+
     <Footer />
+    </Router>
     </div>
+
   );
 }
 
